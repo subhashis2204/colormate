b6ada47cdf0538e4908146090369ab9aa3377ecf
subhashis2204 <subhashispaul2204@gmail.com>
2024-08-04 10:15:32 +0530

    fixed filter issue

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ colorFilter: 'none' });
-  });
-  
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: () => {
-        const currentFilter = document.documentElement.style.filter;
-        if (currentFilter) {
-          document.documentElement.style.filter = '';
-        } else {
-          document.documentElement.style.filter = 'url(#protanopia)';
-        }
+  chrome.storage.sync.set({ colorFilter: 'none' });
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      const currentFilter = document.documentElement.style.filter;
+      if (currentFilter) {
+        document.documentElement.style.filter = '';
+      } else {
+        document.documentElement.style.filter = 'url(#protanopia)';
       }
-    });
+    }
   });
-  
+});
--- popup.js
+++ popup.js
@@ -7,25 +7,63 @@ function applyFilter(type) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      function: setColorFilter,
+      func: setColorFilter,
       args: [type]
     });
   });
 }
 
 function setColorFilter(type) {
+  const svgFilters = `
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <filter id="protanopia">
+          <feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0
+                                                0.558, 0.442, 0, 0, 0
+                                                0, 0.242, 0.758, 0, 0
+                                                0, 0, 0, 1, 0" />
+        </filter>
+        <filter id="deuteranopia">
+          <feColorMatrix type="matrix" values="0.625, 0.375, 0, 0, 0
+                                                0.7, 0.3, 0, 0, 0
+                                                0, 0.3, 0.7, 0, 0
+                                                0, 0, 0, 1, 0" />
+        </filter>
+        <filter id="tritanopia">
+          <feColorMatrix type="matrix" values="0.95, 0.05, 0, 0, 0
+                                                0, 0.433, 0.567, 0, 0
+                                                0, 0.475, 0.525, 0, 0
+                                                0, 0, 0, 1, 0" />
+        </filter>
+      </defs>
+    </svg>
+  `;
+  
+  const existingDiv = document.getElementById('color-blindness-filters');
+  if (existingDiv) {
+    existingDiv.remove();
+  }
+
+  const div = document.createElement('div');
+  div.id = 'color-blindness-filters';
+  div.style.display = 'none';
+  div.innerHTML = svgFilters;
+  document.body.appendChild(div);
+
+  let filterValue = '';
   switch (type) {
     case 'protanopia':
-      document.documentElement.style.filter = 'url(#protanopia)';
+      filterValue = 'url(#protanopia)';
       break;
     case 'deuteranopia':
-      document.documentElement.style.filter = 'url(#deuteranopia)';
+      filterValue = 'url(#deuteranopia)';
       break;
     case 'tritanopia':
-      document.documentElement.style.filter = 'url(#tritanopia)';
+      filterValue = 'url(#tritanopia)';
       break;
     default:
-      document.documentElement.style.filter = '';
+      filterValue = '';
       break;
   }
+  document.documentElement.style.filter = filterValue;
 }
